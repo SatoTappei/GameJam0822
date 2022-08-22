11dac41bd6bb3be96880036824ecbedba321f6d8
RK <riku.kuu0421@gmail.com>
2022-08-22 12:44:25 +0900

    hp追加途中です

--- Assets/Ohashi/Playercontrolle.cs
+++ Assets/Ohashi/Playercontrolle.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Playercontrolle : MonoBehaviour
 {
-    [SerializeField] float _moveSpeed;
+    [Header("移動スピード")]
+    [Tooltip("移動スピード")] [SerializeField] float _moveSpeed;
+    [Header("playerのヒットポイント")]
+    [Tooltip("playerのヒットポイント")] [SerializeField] int _hp = 2;
+
     Vector3 _dir = new Vector3(0, 0, 0);
     Rigidbody _rb;
+    
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
@@ -17,9 +23,27 @@ public class Playercontrolle : MonoBehaviour
     }
     void Update()
     {
+        Move();//移動の入力
+        Hit();
+    }
+    void Move()
+    {
         float _moveZ = Input.GetAxisRaw("Horizontal1");
         float _moveX = Input.GetAxisRaw("Vertical1");
         _dir = new Vector3(0, _moveX, _moveZ);
-
+    }
+    void Hit()
+    {
+        if(_hp <= 0)
+        {
+            SceneManager.LoadScene("result画面のscene名");
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "弾のタグ名")
+        {
+            _hp--;
+        }
     }
 }
